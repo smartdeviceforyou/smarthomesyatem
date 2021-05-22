bc2b4fa71a794a0c4ffd69b14ece75da6167328f
WADDE3161 <61944197+WADDE3161@users.noreply.github.com>
2021-05-22 07:37:31 +0530

    Create my.js

---
+++ my.js
@@ -0,0 +1,54 @@
+function setRandomLink() {
+   var randLink = document.getElementById('randLink');
+ 
+   var theLinks = new Array(
+      'http://answers.yahoo.com',
+      'http://www.imdb.com/name/nm0005453',
+      'http://www.funnycoolstuff.com/images/paris-hilton-sexy.jpg',
+      'http://www.thesuperficial.com/archives/2005/12/26/jenifer_love_hewitt_is_kind_to.html',
+      'http://www.youtube.com/v/8MrIHH2zdBc'
+   );
+ 
+   var arrSize = theLinks.length;
+ 
+   var randNo = Math.random();
+   randNo = Math.floor(randNo * arrSize);
+ 
+   randLink.href = theLinks[randNo];
+}function setRandomLink() {
+   var randLink = document.getElementById('randLink');
+ 
+   var theLinks = new Array(
+      'http://answers.yahoo.com',
+      'http://www.imdb.com/name/nm0005453',
+      'http://www.funnycoolstuff.com/images/paris-hilton-sexy.jpg',
+      'http://www.thesuperficial.com/archives/2005/12/26/jenifer_love_hewitt_is_kind_to.html',
+      'http://www.youtube.com/v/8MrIHH2zdBc'
+   );
+ 
+   var arrSize = theLinks.length;
+ 
+   var randNo = Math.random();
+   randNo = Math.floor(randNo * arrSize);
+ 
+   randLink.href = theLinks[randNo];
+}
+
+function setRandomLink() {
+   var randLink = document.getElementById('randLink');
+ 
+   var theLinks = new Array(
+      'http://answers.yahoo.com',
+      'http://www.imdb.com/name/nm0005453',
+      'http://www.funnycoolstuff.com/images/paris-hilton-sexy.jpg',
+      'http://www.thesuperficial.com/archives/2005/12/26/jenifer_love_hewitt_is_kind_to.html',
+      'http://www.youtube.com/v/8MrIHH2zdBc'
+   );
+ 
+   var arrSize = theLinks.length;
+ 
+   var randNo = Math.random();
+   randNo = Math.floor(randNo * arrSize);
+ 
+   randLink.href = theLinks[randNo];
+}
